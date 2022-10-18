fb788d61e073e9ba022fb9aee6a9e4e0b439d7ae
Eli Wood <mreliwood1@gmail.com>
2022-10-18 14:35:07 -0700

    fetching html test

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-try {
-    window.odata
-    .o(process.env.API_URL)
-    .get()
-    .query({ Authorization: process.env.API_KEY })
-    .then((data) => console.log("DATA: ", data));
-} catch {
-    console.log('ERROR LOADING DATA');
-};
+// try {
+//     window.odata
+//     .o(process.env.API_URL)
+//     .get()
+//     .query({ Authorization: process.env.API_KEY })
+//     .then((data) => console.log("DATA: ", data));
+// } catch {
+//     console.log('ERROR LOADING DATA');
+// };
 
 // api does not allow fetch from local file system
 // try using insomnia instead
@@ -15,3 +15,23 @@ try {
 // try using insomnia
 
 // include gzip in header
+
+const propertiesDiv = document.getElementById("render-past-sales");
+
+// search products
+const getProperties = () => {
+
+  let request = new XMLHttpRequest();
+
+  request.open("GET", "https://www.compass.com/agents/samantha-burnstead/", true);
+
+  request.onload = () => {
+    const doc = new DOMParser().parseFromString(request.responseText, "text/html");
+    propertiesDiv.innerHTML = doc.getElementsByClassName("closedDeals-container").innerHTML;
+  };
+
+  request.send();
+
+};
+
+getProperties();
